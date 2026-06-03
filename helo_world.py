dc821633be472c95513195aa3a7201f8ee1c9bf1
x2lx.dev <x2lx.dev@gmail.com>
2026-06-03 21:14:44 +0200

    Add files via upload

---
+++ helo_world.py
@@ -0,0 +1,3 @@
+celsius = float(input("entrez des degres celsius: "))
+
+print("Il fais", celsius * 9 / 5 + 32)
---
+++ projet.py
@@ -0,0 +1,5 @@
+kmh = float(input("Entrez une vitesse en km/h : "))
+mph = kmh * 0,621371
+
+
+print(f"{kmh} km/h équivaut à {mph} mph.")
